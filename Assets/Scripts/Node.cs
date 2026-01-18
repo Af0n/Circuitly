a8f3740162022583a6be7134e9da408af5f93ba0
afonc <37196107+Af0n@users.noreply.github.com>
2026-01-17 17:11:54 -0800

    Implementing Node

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -2,6 +2,25 @@ using UnityEngine;
 
 public class Node : MonoBehaviour
 {
-    public Node[] In;
-    public Node[] Out;
+    [Tooltip("Nodes where the flow of sparks come into from")]
+    public Node[] InNodes;
+    [Tooltip("Nodes where the flow of sparks goes out to")]
+    public Node[] OutNodes;
+    [Tooltip("Can also be considered as a 'CanTraverse' flag")]
+    public bool IsClosed;
+    
+    // number of closed nodes in the out list
+    public int NumClosedOutNodes
+    {
+        get
+        {
+            int count = 0;
+            foreach (Node n in OutNodes)
+            {
+                // if node is closed, add 1. if not, don't count it.
+                count += n.IsClosed ? 1 : 0;
+            }
+            return count;
+        }
+    }
 }
